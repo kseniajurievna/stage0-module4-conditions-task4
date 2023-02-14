7e5ba14c6672d58aaf76d6ae658d0af2f7de8d38
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-13 19:29:03 -0800

    ternary1

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -1,6 +1,8 @@
 package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
-    public void printGreatest(int first, int second) {
+    public static void printGreatest(int first, int second) {
+        int result = first > second ? first : second;
+        System.out.println(result);
     }
 }
